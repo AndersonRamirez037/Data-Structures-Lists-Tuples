95ffa516397b8ea3f709f1a6d9758fa457c11933
AndersonRamirez037 <andersongamer037@gmail.com>
2023-07-12 19:20:30 -0500

    9. Find the max

---
+++ 9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    biggest_integer = 0
+    for i in my_list:
+        if i > biggest_integer:
+            biggest_integer = i
+    return biggest_integer
